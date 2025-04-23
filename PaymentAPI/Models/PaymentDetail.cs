76c9d85336a89839e0c2fe407e8acfd019469dba
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-23 20:36:05 -0300

    Created Data and models folder. maked migration and get the extensios for EntittyFramework

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        public string CardOwnerName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string ExpirationDate { get; set; } = string.Empty;
+        public string SecurityCode { get; set; } = string.Empty;
+    }
+}
